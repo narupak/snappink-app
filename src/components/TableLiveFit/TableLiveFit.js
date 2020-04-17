a0d739e630c5bc9895aad7c428b15dfa3f78320d
TheLastHope <puchit@windowslive.com>
2020-04-17 14:01:17 +0700

    commit all

--- src/components/TableLiveFit/TableLiveFit.js
+++ src/components/TableLiveFit/TableLiveFit.js
@@ -2,8 +2,20 @@ import React, { Component } from "react";
 import DataTable, { createTheme } from "react-data-table-component";
 import IconSort from "./icons";
 import "./styles.css";
-import { Pencil, TrashFill } from "react-bootstrap-icons";
+import { Pencil, TrashFill, Search } from "react-bootstrap-icons";
 import Pagination from "react-js-pagination";
+import axios from "axios";
+import Moment from "react-moment";
+import logoNoData from "./icons/partner-nodata-ico.5048444f.svg";
+import { Link } from "react-router-dom";
+import {
+  InputGroup,
+  Button,
+  FormControl,
+  Row,
+  Col,
+  Form,
+} from "react-bootstrap";
 
 const customStyles = {
   headCells: {
@@ -34,24 +46,21 @@ const columns = [
   {
     name: "TRAINER",
     selector: "trainer",
-    sortable: true,
     width: "17%",
   },
   {
-    name: <div className="text-center ml-3">PRICE (THB)</div>,
+    name: <div className="text-center">PRICE (THB)</div>,
     selector: "price",
-    sortable: true,
     center: true,
   },
   {
-    name: <div className="text-center ml-3">PRODUCTION TIME</div>,
+    name: <div className="text-center">PRODUCTION TIME</div>,
     selector: "productionTime",
-    sortable: true,
     center: true,
   },
   {
     name: "LIVE STATUS",
-    sortable: true,
+    center: true,
     cell: (row) => {
       if (row.liveStatus === "Live Now") {
         return <span style={{ color: "rgb(59, 160, 128)" }}>Live Now</span>;
@@ -68,26 +77,44 @@ const columns = [
   },
   {
     name: "LIVE TIME",
-    selector: "liveTime",
-    sortable: true,
+    center: true,
+    cell: (row) => {
+      return (
+        <div>
+          <Moment format="HH:MM">{row.liveOpenTime}</Moment> -{" "}
+          <Moment format="HH:MM">{row.liveCloseTime}</Moment>
+        </div>
+      );
+    },
   },
   {
     name: "LIVE DATE",
     selector: "liveDate",
-    sortable: true,
     center: true,
+    cell: (row) => {
+      return (
+        <div>
+          <Moment format="ll">{row.liveOpenTime}</Moment>
+        </div>
+      );
+    },
   },
   {
     name: "OPERATION",
     center: true,
+    width: "12%",
     cell: (row) => {
       return (
         row.liveStatus === "Soon" && (
           <div className="text-center">
-            <button type="button" className="btn btn btn-link btn-sm">
-              <Pencil style={{ color: "rgb(30, 48, 100)" }} />
-            </button>
-            <button type="button" className="btn btn btn-link btn-sm">
+            <Link to={`/createClass/${row.classId}`}>
+              <button type="button" className="btn btn btn-link btn-sm">
+                <Pencil style={{ color: "rgb(30, 48, 100)" }} />
+              </button>
+            </Link>
+            <button onClick={()=>{
+          
+            }} type="button" className="btn btn btn-link btn-sm ml-4">
               <TrashFill style={{ color: "rgb(30, 48, 100)" }} />
             </button>
           </div>
@@ -110,90 +137,188 @@ export class Table extends Component {
     this.state = {
       allData: [],
       page: 1,
-      size: 10,
-      search: this.props.search,
-      obj_flag: "",
-      activePage: 15,
+      sizePage: 10,
+      search: "",
+      objFlag: "",
+      activePage: 1,
+      totalPage: 0,
+      sort: "",
+      url: "https://api-staging.snappink.com/api/gym_class/v1/filter?",
+      chk: false
     };
   }
 
   componentDidMount() {
+    this.handleTotalPage();
     this.handleTable();
   }
 
-  componentWillReceiveProps() {
-    console.log(this.props.search);
-    this.handleTable();
-  }
+  handleSearch = (e) => {
+    this.setState(
+      {
+        search: e.target.value,
+      },
+      () => {
+        this.handleTable();
+      }
+    );
+  };
 
-  handlePageChange(pageNumber) {
-    console.log(`active page is ${pageNumber}`);
-    this.setState({ activePage: pageNumber });
-  }
+  handleFilter = (e) => {
+    this.setState(
+      {
+        objFlag: e.target.value,
+      },
+      () => {
+        this.handleTable();
+      }
+    );
+  };
 
-  handleTable = () => {
+  handleSort = (obj, sort) => {
+    this.setState(
+      {
+        sort: sort,
+      },
+      () => {
+        console.log(this.state.sort);
+        this.handleTable();
+      }
+    );
+  };
+
+  handlePageChange = (pageNumber) => {
+    this.setState({ page: pageNumber }, () => {
+      this.handleTable();
+    });
+  };
+
+  handleTotalPage = async () => {
     let gymId = localStorage.getItem("user");
     gymId = JSON.parse(gymId).gym_id;
     gymId = "gym_id=" + gymId;
+    let url = this.state.url + gymId;
+    try {
+      const response = await axios.get(url);
+      response.data.result.total &&
+        this.setState({ totalPage: response.data.result.total });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  handleTable = async () => {
     let page = "&page=" + this.state.page;
-    let size = "&size=" + this.state.size;
-    let url =
-      "https://api-staging.snappink.com/api/gym_class/v1/filter?" +
-      gymId +
-      page +
-      size +
-      "&obj_flag=";
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.message === "Get gym class success.") {
-          console.log(data);
-          const tmp = data.result.items.map((val, index) => {
-            return {
-              id: val.id,
-              index: index + 1,
-              className: val.name,
-              trainer: val.trainer_name,
-              price: val.snappink_price,
-              productionTime: val.exp_day_amt,
-              liveStatus: val.live_status,
-              liveTime: "-",
-              liveDate: "-",
-            };
-          });
-          this.setState({ allData: tmp });
-        }
-      })
-      .catch(console.log);
+    let size = "&size=" + this.state.sizePage;
+    let search = this.state.search ? "&s=" + this.state.search : "";
+    let objFlag = this.state.objFlag ? "&obj_flag=" + this.state.objFlag : "";
+    let sort = "";
+    if (this.state.sort) {
+      sort = this.state.sort === "asc" ? "&sort_by=name" : "&sort_by=-name";
+    }
+    let gymId = localStorage.getItem("user");
+    gymId = JSON.parse(gymId).gym_id;
+    gymId = "gym_id=" + gymId;
+    let url = this.state.url + gymId + page + size + search + objFlag + sort;
+    try {
+      const response = await axios.get(url);
+      if (response.status === 200) {
+        console.log(response);
+        const res = response.data.result.items.map((val, index) => {
+          return {
+            id: val.id,
+            index: this.state.page * 10 + (index + 1) - 10,
+            className: val.name,
+            trainer: val.trainer_name,
+            price: val.snappink_price,
+            productionTime: val.exp_day_amt,
+            liveStatus: val.live_status,
+            liveOpenTime: val.live_open_time,
+            liveCloseTime: val.live_close_time,
+            liveDate: val.live_day_name,
+            classId : val._id,
+          };
+        });
+        this.setState({ allData: res });
+        this.setState({ totalPage: response.data.result.total });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  editClass = (id) => {
-    console.log(id);
-  };
-
-  removeClass = (id) => {
+  deleteClass = (id) => {
     console.log(id);
   };
 
   render() {
     return (
       <div>
+        <Row>
+          <Col></Col>
+          <Col xs={3}>
+            <InputGroup className="mb-3 mt-3">
+              <InputGroup.Prepend
+                style={{ border: "1px solid rgb(206, 212, 218)" }}
+              >
+                <Button
+                  style={{ backgroundColor: "white", pointerEvents: "none" }}
+                >
+                  <Search style={{ color: "rgb(30, 48, 100)" }} />
+                </Button>
+              </InputGroup.Prepend>
+              <FormControl
+                onChange={this.handleSearch}
+                aria-label="Text input with checkbox"
+              />
+            </InputGroup>
+          </Col>
+          <Col xs={2}>
+            <Form.Control
+              onChange={this.handleFilter}
+              className="mb-3 mt-3"
+              as="select"
+            >
+              <option value="">All</option>
+              <option value="300">Live Now</option>
+              <option value="0">Soon</option>
+              <option value="500">Lived</option>
+            </Form.Control>
+          </Col>
+        </Row>
         <DataTable
           sortIcon={<IconSort />}
-          columns={columns}
+          columns={
+            columns
+            
+          }
           data={this.state.allData}
           theme="solarized"
           customStyles={customStyles}
           noHeader={true}
           pagination={false}
-          noDataComponent={"Not data"}
+          onSort={this.handleSort}
+          sortFunction={(rows, field, direction) => {
+            return rows;
+          }}
+          noDataComponent={
+            <div
+              className="text-center mt-5"
+              style={{ width: "100%", height: "200px" }}
+            >
+              <div className="col-12">
+                <img src={logoNoData} alt={logoNoData} />
+              </div>
+              <div className="col-12">No Data</div>
+            </div>
+          }
         />
         <div style={{ backgroundColor: "white" }} className="card-footer">
           <Pagination
-            activePage={this.state.activePage}
-            itemsCountPerPage={10}
-            totalItemsCount={450}
-            pageRangeDisplayed={5}
+            activePage={this.state.page}
+            itemsCountPerPage={this.state.sizePage}
+            totalItemsCount={this.state.totalPage}
+            pageRangeDisplayed={6}
             onChange={this.handlePageChange.bind(this)}
           />
         </div>
